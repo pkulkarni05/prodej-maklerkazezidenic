1776c3b990af2992a3abe7eadadca61cfd4250f8
Prakash Kulkarni <kulkarni.prakash@gmail.com>
2025-11-06 11:39:40 +0100

    updated message on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,17 @@ import KulkarniConsultingNote from "../components/KulkarniConsultingNote";
 export default function Home() {
   return (
     <div style={{ padding: "2rem", maxWidth: "600px", margin: "auto" }}>
-      <h1>Vítejte na stránce s prodejni nemovitostí</h1>
+      <h2>
+        Vítejte na stránce k prodeji nemovitostí <br></br>z portfolia Jany
+        Bodákové
+      </h2>
       <p>
-        Děkujeme, že jste navštívili naši stránku pro prodejni nemovitostí. Pro
-        přístup k prodejni formuláři prosím použijte konkrétní odkaz, který jste
-        obdrželi.
+        Děkujeme, že jste navštívili naši stránku s dokumenty pro prodej
+        nemovitostí v Brně a okolí. Pro přístup k formulářům prosím použijte
+        konkrétní odkaz, který jste obdrželi.
       </p>
       <p>
-        Pokud odkaz nemáte, obraťte se na realitního makléře, který vám ho
+        Pokud odkaz nemáte, obraťte se na realitní makléřku, která vám ho ráda
         poskytne.
       </p>
       <KulkarniConsultingNote />
